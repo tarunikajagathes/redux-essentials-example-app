e806bb57d92ac6313b8e8e978b86a1a2ef3baf49
tarunikajagathes <tarunika.jagathes@aspiresys.com>
2022-06-23 18:21:11 +0530

    Implemented redux data

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ import {
 import { Navbar } from './app/Navbar'
 import { AddPostForm } from './features/posts/AddPostForm'
 
-import { PostsList } from './features/posts/postsList'
+import { PostsList } from './features/posts/postsList';
+import {SinglePostPage} from './features/posts/SinglePostPage';
+import {EditPostForm} from './features/posts/EditPostForm';
 
 function App() {
   return (
@@ -27,6 +29,8 @@ function App() {
               </React.Fragment>
             )}
           />
+          <Route exact path="/posts/:postId" component={SinglePostPage} />
+          <Route exact path="/editPost/:postId" component={EditPostForm} />
           <Redirect to="/" />
         </Switch>
       </div>
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, nanoid } from '@reduxjs/toolkit'
 
 const initialState = [
   { id: '1', title: 'First Post!', content: 'Hello!' },
@@ -9,12 +9,35 @@ const postsSlice = createSlice({
   name: 'posts',
   initialState,
   reducers: {
+    postAdded: {
+      reducer(state, action) {
+        state.push(action.payload)
+      },
+      prepare(title, content, userId) {
+        return {
+          payload: {
+            id: nanoid(),
+            title,
+            content,
+            user:userId
+          }
+        }
+      }
+    },
     postAdded(state, action) {
         state.push(action.payload)
+      },
+      postUpdated(state, action) {
+        const { id, title, content } = action.payload
+        const existingPost = state.find(post => post.id === id)
+        if (existingPost) {
+          existingPost.title = title
+          existingPost.content = content
+        }
       }
   }
 })
 
-export const { postAdded } = postsSlice.actions;
+export const { postAdded, postUpdated } = postsSlice.actions;
 
 export default postsSlice.reducer
